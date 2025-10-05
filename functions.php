cd928015c7e03a41b1d65b44326b2d05f4b0ec1a
AlejandroLavandeira <alejandro.lavandeira@rai.usc.es>
2025-10-05 22:37:55 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -55,3 +55,8 @@ if (class_exists('PressWind\PWVite')) {
         is_ts: false
     );
 }
+
+function tejamos_sonrisas_add_woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'tejamos_sonrisas_add_woocommerce_support' );
